78a6f136e8aa25ec7040457ca8143da3013794be
Morozqq <aslan.dzheleubaj.04@gmail.com>
2024-02-27 23:01:55 +0600

    update

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const bcrypt = require("bcrypt");
 const session = require("express-session");
 const mongoose = require("mongoose");
 const path = require("path");
-const api = require("./api");
 const axios = require("axios");
+const fs = require("fs");
+const multer = require("multer");
+const api = require("./api");
 
 const app = express();
 const port = 3000;
@@ -28,6 +30,17 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+const newsSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: String,
+    title_ru: String,
+    description: String,
+    description_ru: String,
+    image: String,
+});
+
+const News = mongoose.model("News", newsSchema);
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -37,6 +50,23 @@ app.use(
         saveUninitialized: true,
     })
 );
+
+const uploadDir = path.join(__dirname, "./uploads");
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        const sanitizedDate = new Date().toISOString().replace(/:/g, "-");
+        cb(null, sanitizedDate + "_" + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 // Authentication Middleware
 const requireAuth = async (req, res, next) => {
@@ -142,22 +172,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.get("/admin", requireAuth, async (req, res) => {
-    try {
-        // Fetch the users from the database (replace this with your actual logic)
-        const users = await User.find();
-
-        res.render("admin", {
-            username: req.user.username,
-            users: users,
-            user: {},
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
 // Main Page - Frontend with Authorization Middleware
 app.get("/main", requireAuth, async (req, res) => {
     try {
@@ -197,6 +211,14 @@ const headers = {
     "Content-Type": "application/json",
     Authorization: "Bearer 3b5a9d07-4b77-4be4-bf5d-f9ff3d6cb7b6",
 };
+
+// Middleware to add /admin to news routes (except GET)
+app.use("/news", (req, res, next) => {
+    if (req.method !== "GET") {
+        req.url = "/admin" + req.url;
+    }
+    next();
+});
 
 app.get("/", requireAuth, async function (req, res) {
     try {
@@ -253,6 +275,25 @@ app.get("/", requireAuth, async function (req, res) {
     }
 });
 
+app.get("/admin", requireAuth, async (req, res) => {
+    try {
+        // Fetch the users from the database (replace this with your actual logic)
+        const users = await User.find();
+        const news = await News.find(); // Added to fetch news for admin page
+
+        res.render("admin", {
+            username: req.user.username,
+            users: users,
+            news: news, // Added to pass news data to the admin page
+            user: {},
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Add User - Backend
 app.post("/admin/addUser", requireAuth, async (req, res) => {
     if (!req.user.isAdmin) {
         return res.status(403).send("Forbidden");
@@ -322,6 +363,187 @@ app.post("/admin/editUser", requireAuth, async (req, res) => {
             isAdmin: isAdmin === "on",
         });
 
+        res.redirect("/admin");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// News Page - Backend
+app.get("/news", requireAuth, async (req, res, next) => {
+    try {
+        const news = await News.find().exec();
+        const selectedLanguage = req.query.language || "en";
+
+        res.render("news", { news, selectedLanguage });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Add News - Backend
+app.post(
+    "/admin/create/:newsId",
+    requireAuth,
+    upload.single("newsImage"),
+    (req, res, next) => {
+        const news = new News({
+            _id: new mongoose.Types.ObjectId(),
+            title: req.body.title,
+            title_ru: req.body.title_ru,
+            description: req.body.description,
+            description_ru: req.body.description_ru,
+            // Use the link if provided, otherwise use the file path
+            image: req.body.newsImage || (req.file ? req.file.path : null),
+        });
+
+        news.save()
+            .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                    message: "News created successfully",
+                    news: result,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({ error: err });
+            });
+    }
+);
+
+// Get News by ID - Backend
+app.get("/news/:newsId", requireAuth, (req, res, next) => {
+    const id = req.params.newsId;
+    News.findById(id)
+        .exec()
+        .then((doc) => {
+            console.log(doc);
+            res.status(200).json(doc);
+        })
+        .catch((err) => console.log(err));
+    res.status(500);
+});
+
+// Update News by ID - Backend
+app.patch("/news/:newsId", requireAuth, (req, res, next) => {
+    const id = req.params.newsId;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid newsId format" });
+    }
+
+    const updateOps = {};
+
+    if (req.body.title) {
+        updateOps.title = req.body.title;
+    }
+
+    if (req.body.title_ru) {
+        updateOps.title_ru = req.body.title_ru;
+    }
+
+    if (req.body.description) {
+        updateOps.description = req.body.description;
+    }
+
+    if (req.body.description_ru) {
+        updateOps.description_ru = req.body.description_ru;
+    }
+
+    News.updateOne({ _id: id }, { $set: updateOps })
+        .exec()
+        .then((result) => {
+            res.status(200).json({ message: "Updated", result });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
+
+// Delete News by ID - Backend
+app.delete("/news/:newsId", requireAuth, (req, res, next) => {
+    const id = req.params.newsId;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid newsId format" });
+    }
+
+    News.findOneAndDelete({ _id: id })
+        .exec()
+        .then((result) => {
+            if (result) {
+                res.status(200).json({ message: "Deleted", result });
+            } else {
+                res.status(404).json({ message: "Not found" });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
+app.get("/admin/editNews/:newsId", requireAuth, async (req, res) => {
+    try {
+        const newsId = req.params.newsId;
+        const news = await News.findById(newsId);
+
+        if (!news) {
+            return res.status(404).send("News not found");
+        }
+
+        res.render("editNews", { news: news });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Update News Page - Backend
+app.post(
+    "/admin/editNews/:newsId",
+    requireAuth,
+    upload.single("newsImage"),
+    (req, res, next) => {
+        const newsId = req.params.newsId;
+
+        if (!mongoose.Types.ObjectId.isValid(newsId)) {
+            return res.status(400).json({ message: "Invalid newsId format" });
+        }
+
+        const updateOps = {
+            title: req.body.title,
+            title_ru: req.body.title_ru,
+            description: req.body.description,
+            description_ru: req.body.description_ru,
+            image: req.file ? req.file.path : null,
+        };
+
+        News.findByIdAndUpdate(newsId, { $set: updateOps }, { new: true })
+            .exec()
+            .then((updatedNews) => {
+                res.redirect("/admin");
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({ error: err });
+            });
+    }
+);
+
+// Delete News Page - Backend
+app.post("/admin/deleteNews/:newsId", requireAuth, async (req, res) => {
+    if (!req.user.isAdmin) {
+        return res.status(403).send("Forbidden");
+    }
+
+    const newsIdToDelete = req.params.newsId;
+
+    try {
+        await News.findOneAndDelete({ _id: newsIdToDelete });
         res.redirect("/admin");
     } catch (error) {
         console.error(error);
